59d4bbacc8c63938db4e2b904afc3a785bf8aeb1
Alirezabideli <alirezabideli97@gmail.com>
2018-12-22 12:44:38 +0330

    Add forgotten layout for DoneFragment

--- app/src/main/java/com/example/admin/software_1/controllers/DoneFragment.java
+++ app/src/main/java/com/example/admin/software_1/controllers/DoneFragment.java
@@ -1,8 +1,30 @@
 package com.example.admin.software_1.controllers;
 
-/**
- * Created by ADMIN on 12/22/2018.
- */
 
-public class DoneFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.admin.software_1.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DoneFragment extends Fragment {
+
+
+    public DoneFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_done, container, false);
+    }
+
 }
