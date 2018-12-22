b0f0a0958b9d3ca2cd51f04644b02633f8363dcd
Alirezabideli <alirezabideli97@gmail.com>
2018-12-22 13:22:41 +0330

    Add two models  Task and TaskLab Task for storing title,description,date and hour TaskLab for making TasksList and return the taskList

--- app/src/main/java/com/example/admin/software_1/models/Task.java
+++ app/src/main/java/com/example/admin/software_1/models/Task.java
@@ -1,8 +1,54 @@
 package com.example.admin.software_1.models;
 
+import java.util.Date;
+
 /**
  * Created by ADMIN on 12/22/2018.
  */
 
 public class Task {
+
+
+
+    private String mTitle;
+    private String mDescription;
+    private Date mDate;
+    private Date mHour;
+
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Date getHour() {
+        return mHour;
+    }
+
+    public void setHour(Date hour) {
+        mHour = hour;
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/admin/software_1/models/TaskLab.java
+++ app/src/main/java/com/example/admin/software_1/models/TaskLab.java
@@ -1,8 +1,32 @@
 package com.example.admin.software_1.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ADMIN on 12/22/2018.
  */
 
 public class TaskLab {
+
+
+    private static final TaskLab instance =new TaskLab();
+
+
+    private List<Task> mTasks=new ArrayList<>();
+
+    public static TaskLab getInstance() {
+        return instance;
+    }
+
+    public void addTask(Task task)
+    {
+        mTasks.add(task);
+    }
+
+    public List<Task> getTaskList()
+    {
+        return mTasks;
+    }
 }
+
